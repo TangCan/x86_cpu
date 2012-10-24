75c1251fee5f539e46cc3deed6320694da6ae785
Richard.Tang <Tang_Can@qq.com>
2012-10-24 17:32:36 +0800

    fix cannot create directory issue

---
+++ public/Conf/config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+  'APP_DEBUG' => true, //开启调试模式
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'a.db.shared.orchestra.io',
+  'DB_NAME' => 'db_9f354acd',
+  'DB_USER' => 'user_9f354acd',
+  'DB_PWD' => 'w0rKx.^!FY^3%Q',
+  'DB_PORT' => '3306',
+  'DB_PREFIX' => 'x86_cpu_',
+);
+?>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
-  require '../ThinkPHP/ThinkPHP.php';
+define('THINK_PATH','../ThinkPHP/');
+define('APP_NAME', 'x86_cpu');
+define('APP_PATH', '.');
+require '../ThinkPHP/ThinkPHP.php';
+App::run();
 ?>
